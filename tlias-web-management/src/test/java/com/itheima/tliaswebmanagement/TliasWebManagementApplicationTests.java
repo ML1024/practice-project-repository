287b2c5c886c998c57ca46bd4298cd7fd374f61d
mmmml <lucy83844@gmail.com>
2024-01-14 00:50:49 +0800

    add GlobalExceptionHandler

--- tlias-web-management/src/test/java/com/itheima/tliaswebmanagement/TliasWebManagementApplicationTests.java
+++ tlias-web-management/src/test/java/com/itheima/tliaswebmanagement/TliasWebManagementApplicationTests.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 
-@SpringBootTest
+//@SpringBootTest
 class TliasWebManagementApplicationTests {
 	/**
 	 * 生成JWT
@@ -24,8 +24,8 @@ class TliasWebManagementApplicationTests {
 
 		String jwt = Jwts.builder()
 				.signWith(HS256, "itheima") //指定数字签名的算法 HS256，指定数字签名的秘钥 itheima
-				.setClaims(claims)  //设置自定义的内容(载荷) → Java程序可以把自定义的数据封装到Map集合当中，也就是key:value形式的键值对
-				.setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000)) //设置令牌的有效期 这里设置了令牌的有效期为1个小时
+				.setClaims(claims)  //设置自定义的内容(载荷) → Java程序可以把自定义的数据封装到Map集合当中
+				.setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000)) //设置令牌的有效期为1h
 				.compact();//调用compact方法，可以拿到一个字符串类型的返回值 → 就是生成的JWT令牌
 
 		System.out.println(jwt);
